fb9059825b7a0bf40829921658b546c3d0cb35b3
Michael-Mbaya <gikundamike@gmail.com>
2020-07-18 01:18:01 +0300

    link js and test inputs

---
+++ js/names.js
@@ -0,0 +1,20 @@
+function akaned(){
+    var DayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var maleAkanName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    var femaleAkanName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+    var day = parseInt(document.getElementById("day").value);
+    alert(day);
+    var month = parseInt(document.getElementById("month").value);
+    alert(month)
+    var year = parseInt(document.getElementById("year").value);
+    alert(year);
+    var gender = document.getElementById("gender").value;
+    alert(gender);
+
+
+
+
+
+
+}
